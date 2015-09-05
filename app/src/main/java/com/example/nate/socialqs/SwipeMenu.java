abc5eab178eba22c198d33b0bd4a6c819807e5bd
Nate L <nate@Nates-MacBook-Pro.local>
2015-09-05 00:19:13 -0600

    initial swipe commit

---
+++ app/src/main/java/com/example/nate/socialqs/SwipeMenu.java
@@ -0,0 +1,48 @@
+package com.example.nate.socialqs;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Created by nate on 9/4/15.
+ */
+public class SwipeMenu {
+
+    private Context mContext;
+    private List<SwipeMenuItem> mItems;
+    private int mViewType;
+
+    public SwipeMenu(Context context) {
+        mContext = context;
+        mItems = new ArrayList<SwipeMenuItem>();
+    }
+
+    public Context getContext() {
+        return mContext;
+    }
+
+    public void addMenuItem(SwipeMenuItem item) {
+        mItems.add(item);
+    }
+
+    public void removeMenuItem(SwipeMenuItem item) {
+        mItems.remove(item);
+    }
+
+    public List<SwipeMenuItem> getMenuItems() {
+        return mItems;
+    }
+
+    public SwipeMenuItem getMenuItem(int index) {
+        return mItems.get(index);
+    }
+
+    public int getViewType() {
+        return mViewType;
+    }
+
+    public void setViewType(int viewType) {
+        this.mViewType = viewType;
+    }
+}
---
+++ app/src/main/java/com/example/nate/socialqs/SwipeMenuCreator.java
@@ -0,0 +1,9 @@
+package com.example.nate.socialqs;
+
+/**
+ * Created by nate on 9/4/15.
+ */
+public interface SwipeMenuCreator {
+
+    void create(SwipeMenu menu);
+}
